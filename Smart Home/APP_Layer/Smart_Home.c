ddde17fcfb7b88da2f18863c3a44049e47b84389
abdallah-shehawey <shehawey9@gmail.com>
2024-08-31 05:03:16 +0300

    Add UserName And Password with check on UserName

--- Smart Home/APP_Layer/Smart_Home.c
+++ Smart Home/APP_Layer/Smart_Home.c
@@ -25,7 +25,13 @@
 #include "../HAL_Layer/SERVO_MOTOR/Servo_Motor_interface.h"
 #include "../HAL_Layer/SW/SW_interface.h"
 
-#define EEPROM_Status 0x20
+#define NOTPRESSED 0xFF
+
+#define EEPROM_UserNameStatus 0x10
+#define EEPROM_PassWordStatus  0x11
+#define EEPROM_TimeLocation      0x12
+#define EEPROM_USNL_Location    0x13
+#define EEPROM_USNL_Location    0x13
 
 #define EEPROM_UserNameStartLocation 0x21
 #define EEPROM_UserNameEndLocation   0x40
@@ -37,21 +43,25 @@
 #define EEPROM_PassWordMaximumLength  20
 
 char UserName[EEPROM_PassWordMaximumLength];
-char Password[EEPROM_UserNameMaximumLength];
+char Password  [EEPROM_UserNameMaximumLength];
 
-volatile u8 KPD_Press, Error_State = 1, USNL = 0, PWL = 0;
-volatile u8 USN_Get[20];
+volatile u8 KPD_Press, Error_State = 1,USNL = 0, PWL = 0;
+volatile u8 Check[21] ;
+volatile u8 Check_Flag = 1, CheckLength = 0;
 /*
  * USNL ==> User Name Length
  * PWL   ==> PassWord Length
  */
 
-void Clear_Char      (void);
-void Set_UserName(void);
+void Clear_Char            (void);
+void Set_UserName     (void);
+void Set_PassWord      (void);
+void Check_UserName(void);
 
 
 void main(void)
 {
+	USNL = EEPROM_vRead(EEPROM_USNL_Location);
 	/* Initialize CLCD On PORTB And 4Bit Mode And Connected on Low Nibble */
 	CLCD_vInit();
 
@@ -63,7 +73,18 @@ void main(void)
 
 	CLCD_vSetPosition(1, 7);
 	CLCD_vSendString("Welcome");
-	Set_UserName();
+	_delay_ms(500);
+
+	if(EEPROM_vRead(EEPROM_UserNameStatus) == NOTPRESSED)
+	{
+		Set_UserName();
+	}
+	if(EEPROM_vRead(EEPROM_PassWordStatus) == NOTPRESSED)
+	{
+		Set_PassWord();
+	}
+
+	Check_UserName();
 	while (1)
 	{
 	}
@@ -73,6 +94,8 @@ void main(void)
 
 void Set_UserName(void)
 {
+	EEPROM_vWrite(EEPROM_USNL_Location, 0x00);
+	USNL = EEPROM_vRead(EEPROM_USNL_Location);
 	CLCD_vClearScreen();
 	CLCD_vSetPosition(1, 5);
 	CLCD_vSendString("Set UserName");
@@ -99,6 +122,7 @@ void Set_UserName(void)
 			CLCD_vSendString(" : Exit");
 			while(1)
 			{
+				//wait in error page until press enter
 				Error_State = USART_u8ReceiveData(&KPD_Press);
 				if (Error_State == OK)
 				{
@@ -141,7 +165,7 @@ void Set_UserName(void)
 				else
 				{
 					CLCD_vSendData(KPD_Press);
-					EEPROM_vWrite(EEPROM_PassWordStartLocation + USNL, KPD_Press);
+					EEPROM_vWrite(EEPROM_UserNameStartLocation + USNL, KPD_Press);
 					USNL++;
 				}
 			}
@@ -171,15 +195,201 @@ void Set_UserName(void)
 	}while(USNL <= 5);
 	/*___________________________________________________________________________________________________________________*/
 
-	for (u8 i = 0; i < 20 - USNL; i++)
+	for (u8 i = 0; i < 20 - (USNL - 1); i++)
 	{
 		EEPROM_vWrite(EEPROM_UserNameEndLocation - i, 0xFF);
 	}
-
+	EEPROM_vWrite(EEPROM_UserNameStatus, 0x00);
+	EEPROM_vWrite(EEPROM_USNL_Location, USNL);
 }
 
 //======================================================================================================================================//
 
+void Set_PassWord(void)
+{
+	CLCD_vClearScreen();
+	CLCD_vSetPosition(1, 5);
+	CLCD_vSendString("Set PassWord");
+	CLCD_vSetPosition(2, 1);
+	CLCD_vSendString("Maximum char : 20");
+	CLCD_vSendExtraChar(4, 15);  // To Send Enter Symbol
+	CLCD_vSetPosition(4, 16);
+	CLCD_vSendString(" : OK");
+
+	CLCD_vSetPosition(3, 1);
+	PWL = 0;
+	/*Get username from user*/
+	do
+	{
+		//if username is less than 5 char
+		if (PWL != 0)
+		{
+			CLCD_vClearScreen();
+			CLCD_vSendString ("PassWord Must be");
+			CLCD_vSetPosition(2, 1);
+			CLCD_vSendString("More than 5 Char");
+			CLCD_vSendExtraChar(4, 1);
+			CLCD_vSetPosition(4, 2);
+			CLCD_vSendString(" : Exit");
+			while(1)
+			{
+				Error_State = USART_u8ReceiveData(&KPD_Press);
+				if (Error_State == OK)
+				{
+					if (KPD_Press == 0x0D || KPD_Press == 0x0F)
+					{
+						break;
+					}
+				}
+			}
+			CLCD_vClearScreen();
+			CLCD_vSetPosition(1, 5);
+			CLCD_vSendString("Set PassWord");
+			CLCD_vSetPosition(2, 1);
+			CLCD_vSendString("Maximum char : 20");
+			CLCD_vSendExtraChar(4, 15); // To Send Enter Symbol
+			CLCD_vSetPosition(4, 16);
+			CLCD_vSendString(" : OK");
+			CLCD_vSetPosition(3, 1);
+			PWL = 0;
+		}
+		CLCD_vSendCommand(CLCD_DISPLAYON_CURSORON);
+		//get user name from user by using Keypoard
+		while (1)
+		{
+			//get input from Laptop
+			Error_State = USART_u8ReceiveData(&KPD_Press);
+			//if user name length is valid
+			if (Error_State == OK && PWL < 20)
+			{
+				if (KPD_Press == 0x0D || KPD_Press == 0x0F)
+				{
+					CLCD_vSendCommand(CLCD_DISPLAYON_CURSOROFF);
+					break;
+				}
+				else if (KPD_Press == 0x08)
+				{
+					Clear_Char();
+					PWL--;
+				}
+				else
+				{
+					CLCD_vSendData(KPD_Press);
+					EEPROM_vWrite(EEPROM_PassWordStartLocation + PWL, KPD_Press);
+					PWL++;
+				}
+			}
+			//if user name length is more than 20 do no thing exept enter and delete
+			else if (Error_State == OK && PWL >= 20)
+			{
+				if (KPD_Press == 0x0D || KPD_Press == 0x0F)
+				{
+					CLCD_vSendCommand(CLCD_DISPLAYON_CURSOROFF);
+					break;
+				}
+				else if (KPD_Press == 0x08)
+				{
+					Clear_Char();
+					PWL--;
+				}
+				else
+				{
+
+				}
+			}
+			else
+			{
+
+			}
+		}
+	}while(PWL <= 5);
+	/*___________________________________________________________________________________________________________________*/
+
+	for (u8 i = 0; i < 20 - (PWL - 1); i++)
+	{
+		EEPROM_vWrite(EEPROM_PassWordEndLocation - i, 0xFF);
+	}
+	EEPROM_vWrite(EEPROM_PassWordStatus, 0x00);
+}
+
+//======================================================================================================================================//
+
+void Check_UserName(void)
+{
+	CLCD_vClearScreen();
+	CLCD_vSendString("Check UserName");
+	CLCD_vSetPosition(2, 1);
+	CLCD_vSendCommand(CLCD_DISPLAYON_CURSORON);
+	CheckLength = 0, Check_Flag = 1;
+
+	while(1)
+	{
+		Error_State = USART_u8ReceiveData(&KPD_Press);
+		//if user name length is valid
+		if (Error_State == OK && CheckLength < 20)
+		{
+			if (KPD_Press == 0x0D || KPD_Press == 0x0F)
+			{
+				CLCD_vSendCommand(CLCD_DISPLAYON_CURSOROFF);
+				break;
+			}
+			else if (KPD_Press == 0x08)
+			{
+				Clear_Char();
+				CheckLength--;
+			}
+			else
+			{
+				CLCD_vSendData(KPD_Press);
+				Check[CheckLength] = KPD_Press;
+				CheckLength++;
+			}
+		}
+		//if user name length is more than 20 do no thing exept enter and delete
+		else if (Error_State == OK && CheckLength>= 20)
+		{
+			if (KPD_Press == 0x0D || KPD_Press == 0x0F)
+			{
+				CLCD_vSendCommand(CLCD_DISPLAYON_CURSOROFF);
+				break;
+			}
+			else if (KPD_Press == 0x08)
+			{
+				Clear_Char();
+				CheckLength--;
+			}
+			else
+			{
+
+			}
+		}
+		else
+		{
+
+		}
+	}
+	if (CheckLength == USNL)
+	{
+		for(u8 i = 0; i < USNL; i++)
+		{
+			if (Check[i] != EEPROM_vRead(EEPROM_UserNameStartLocation + i))
+			{
+				Check_Flag = 0;
+				CLCD_vClearScreen();
+				CLCD_vSendString("Wrong UserName");
+				_delay_ms(500);
+				Check_UserName();
+			}
+		}
+		if(Check_Flag == 1)
+		{
+			CLCD_vClearScreen();
+			CLCD_vSendString("True UserName");
+		}
+	}
+}
+
+//======================================================================================================================================//
 void Clear_Char()
 {
 	CLCD_vSendCommand(CLCD_SHIFT_CURSOR_LEFT);
@@ -187,3 +397,50 @@ void Clear_Char()
 	CLCD_vSendCommand(CLCD_SHIFT_CURSOR_LEFT);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
